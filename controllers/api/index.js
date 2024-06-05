37c87edd64d13b31a54852fd6f3b763c5cce7797
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-06-05 19:28:27 -0400

    File clean up complete

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const registrationRoutes = require('./registrationRoutes+');
+const registrationRoutes = require('./registrationRoutes');
 
 router.use('/users', userRoutes);
 router.use('/registration', registrationRoutes);
