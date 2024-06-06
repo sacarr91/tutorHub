309d1675dbdb1958a9bd33cc7e1ee46bd4b56ecc
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-06-06 13:07:11 -0400

    Resolved issues with existing seed framework and completed functional tutor table seed configuration

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,13 +3,20 @@ require("dotenv").config({
 })
 const sequelize = require('../config/connection');
 const { User } = require('../models');
+const { Tutor } = require('../models')
 
 const userData = require('./userData.json');
+const tutorData = require('./tutorData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Tutor.bulkCreate(tutorData, {
     individualHooks: true,
     returning: true,
   });
