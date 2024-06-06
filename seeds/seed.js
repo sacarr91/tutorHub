9cc15411176e620472cda75f1db623fa0380bd6b
David Rauf <davidrauf@gmail.com>
2024-06-06 17:52:50 -0400

    small update

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,9 +2,10 @@ require("dotenv").config({
   path: '../.env'
 })
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Tutor } = require('../models');
 
 const userData = require('./userData.json');
+const tutorData = require('/tutorData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -14,6 +15,10 @@ const seedDatabase = async () => {
     returning: true,
   });
 
+  await Tutor.bulkCreate(tutorData, {
+    individualHooks: true,
+    returning: true,
+  });
   process.exit(0);
 };
 
