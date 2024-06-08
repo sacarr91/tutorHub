7f44661bc2fd399aee2c7aea32e32c9baf8f16f8
David Rauf <davidrauf@gmail.com>
2024-06-07 20:19:40 -0400

    updatereg

--- public/js/user-registration.js
+++ public/js/user-registration.js
@@ -21,9 +21,9 @@ function redirectUserForAccountCreation() {
     } else {
         // If userinfo is present, redirect the user based on Account Type
         if (selectedAccountType === "tutor") {
-            location.href = "/public/tutor-registration.html"
+            location.href = "tutor-registration.html"
         } else {
-            location.href = "/public/student-registration.html"
+            location.href = "student-registration.html"
         }
     }
 
