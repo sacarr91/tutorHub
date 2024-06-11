bfc7b3184e7e94079d431827fd18893bafd8a9b2
David Rauf <davidrauf@gmail.com>
2024-06-11 12:30:16 -0400

    workingapi

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,11 +3,13 @@ const userRoutes = require('./userRoutes');
 const tutorRegistrationRoutes = require('./tutorRegistrationRoutes');
 const tutorRoutes = require('./tutorRoutes');
 const instrumentRoutes = require('./instrumentRoutes');
+const tutorInstrument = require('./tutorInstrument');
 
 
 router.use('/users', userRoutes);
 router.use('/tutorRegistration', tutorRegistrationRoutes);
 router.use('/tutors', tutorRoutes);
 router.use('/instruments', instrumentRoutes);
+router.use('/tutorInstrument', tutorInstrument);
 
 module.exports = router;
