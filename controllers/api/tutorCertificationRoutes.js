87b3df1062a2b2a365839c0ab1983d29810f82f4
Joseph Ruff <joegruff16@gmail.com>
2024-06-13 22:56:29 -0400

    Added delete routes for TutorCertification and TutorSpecialty. Updated logic to add functionality to button on edit profile page to delete items.

--- controllers/api/tutorCertificationRoutes.js
+++ controllers/api/tutorCertificationRoutes.js
@@ -1,44 +1,76 @@
-const router = require('express').Router();
-const { User, Certification, TutorCertification } = require('../../models');
+const router = require("express").Router();
+const { User, Certification, TutorCertification } = require("../../models");
 
 // get all tutor_certification records
-router.get('/', async (req, res) => {
-    try {
-       const tutorCertificationData =  await TutorCertification.findAll(
-            {include: [{ all: true, nested: true }]}
-        );
-        res.status(200).json(tutorCertificationData);
-    } catch (err) {res.status(500).json(err)}
+router.get("/", async (req, res) => {
+  try {
+    const tutorCertificationData = await TutorCertification.findAll({
+      include: [{ all: true, nested: true }],
+    });
+    res.status(200).json(tutorCertificationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-
 // get tutor_certification table records by certification id
-router.get('/:id', async (req, res) => {
-    try {
-        const userMatch = await User.findAll({
-            include: [{
-                model: Certification,
-                required: true,
-                where: {
-                    certification_name: req.params.id
-                }
-            }]
-        });
-        res.status(200).json(userMatch);
-    } catch (err) {
-        res.status(500).json({ error: err.message }); }
+router.get("/:id", async (req, res) => {
+  try {
+    const userMatch = await User.findAll({
+      include: [
+        {
+          model: Certification,
+          required: true,
+          where: {
+            certification_name: req.params.id,
+          },
+        },
+      ],
+    });
+    res.status(200).json(userMatch);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // add records to the tutor_certification table
-router.post('/', async(req, res) => {
-    try{
-        await TutorCertification.create({
-            user_id: req.body.user_id,
-            certification_id: req.body.certification_id,
-
-        });
-        res.status(200).json('A new certification has successfully added to your profile!')
-    } catch (err) { res.status(500).json({err}); }
+router.post("/", async (req, res) => {
+  try {
+    await TutorCertification.create({
+      user_id: req.body.user_id,
+      certification_id: req.body.certification_id,
+    });
+    res
+      .status(200)
+      .json("A new certification has successfully added to your profile!");
+  } catch (err) {
+    res.status(500).json({ err });
+  }
 });
 
+// remove records from the tutor_certification table
+
+router.delete("/:user_id/:certification_id", async (req, res) => {
+  try {
+    const deletedCertification = await TutorCertification.destroy({
+      where: {
+        user_id: req.params.user_id,
+        specialty_id: req.params.specialty_id,
+      },
+    });
+    if (deletedCertification === 0) {
+      return res.status(404).json({
+        message:
+          "Sorry, there are no records of this specialty associated with your user",
+      });
+    }
+    return res.status(200).json({ message: "Record deleted successfully" });
+  } catch (err) {
+    res.status(422).json({
+      message:
+        "Sorry, your request could not be processed due to the following error - " +
+        err,
+    });
+  }
+});
 module.exports = router;
--- controllers/api/tutorSpecialtyRoutes.js
+++ controllers/api/tutorSpecialtyRoutes.js
@@ -1,43 +1,74 @@
-const router = require('express').Router();
-const { User, Specialty, TutorSpecialty } = require('../../models');
+const router = require("express").Router();
+const { User, Specialty, TutorSpecialty } = require("../../models");
 
-// get all 
-router.get('/', async(req, res) => {
-    try {
-        const tutorSpecialtyData = await TutorSpecialty.findAll(
-            {include: [{all: true, nested: true}]}
-        );
-        res.status(200).json(tutorSpecialtyData);
-    } catch(err){res.status(500).json(err);}
+// get all
+router.get("/", async (req, res) => {
+  try {
+    const tutorSpecialtyData = await TutorSpecialty.findAll({
+      include: [{ all: true, nested: true }],
+    });
+    res.status(200).json(tutorSpecialtyData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get tutor_specialty by specialty id
-router.get('/:id', async (req, res) => {
-    try {
-        const userMatch = await User.findAll({
-            include: [{
-                model: Specialty,
-                required: true,
-                where: {
-                    specialty_name: req.params.id
-                }
-            }]
-        });
-        res.status(200).json(userMatch);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    const userMatch = await User.findAll({
+      include: [
+        {
+          model: Specialty,
+          required: true,
+          where: {
+            specialty_name: req.params.id,
+          },
+        },
+      ],
+    });
+    res.status(200).json(userMatch);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // add records to the tutor_specialty table
-router.post('/', async(req, res) => {
-    try {
-        await TutorSpecialty.create({
-            user_id: req.body.user_id,
-            specialty_id: req.body.specialty_id,
-        })
-        res.status(200).json({message: `a new specialty has been added to your profile!`})
-    } catch(err){ res.status(500).json(err);}
-})
-
+router.post("/", async (req, res) => {
+  try {
+    await TutorSpecialty.create({
+      user_id: req.body.user_id,
+      specialty_id: req.body.specialty_id,
+    });
+    res
+      .status(200)
+      .json({ message: `a new specialty has been added to your profile!` });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//  remove records from the user_specialty table
+router.delete("/:user_id/:specialty_id", async (req, res) => {
+  try {
+    const deletedSpecialty = await TutorSpecialty.destroy({
+      where: {
+        user_id: req.params.user_id,
+        specialty_id: req.params.specialty_id,
+      },
+    });
+    if (deletedSpecialty === 0) {
+      return res.status(404).json({
+        message:
+          "Sorry, there are no records of this specialty associated with your user",
+      });
+    }
+    return res.status(200).json({ message: "Record deleted successfully" });
+  } catch (err) {
+    res.status(422).json({
+      message:
+        "Sorry, your request could not be processed due to the following error - " +
+        err,
+    });
+  }
+});
 module.exports = router;
